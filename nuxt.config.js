3c5b3e581624ddfe0ebcfb604766e6fde0a3c61d
Satake <satake00157@gmail.com>
2021-03-09 19:17:07 +0900

    とりあえず動かせるようにしたnuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,78 +1,54 @@
-import colors from "vuetify/es5/util/colors";
-
-require("dotenv").config();
+require('dotenv').config();
 export default {
   server: {
     port: process.env.APP_PORT
   },
   env: process.env.API_URL,
+  // ssr: true ユニバーサルモード(サーバサイドレンダリングあり)
+  // ssr: false シングルページアプリケーションモード
   ssr: false,
+
+  // ページヘッダ
   head: {
-    title: "ITS-AMS コントロールパネル",
+    title: 'ITS-AMS コントロールパネル',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "description" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
-  /*
-   ** Global CSS
-   */
+
+  // CSS
   css: ["ress"],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ["@nuxtjs/vuetify", "@nuxt/typescript-build", "@nuxtjs/dotenv"],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: ["@nuxtjs/axios", "@nuxtjs/moment", "nuxt-buefy", { css: false }],
-  /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
-  vuetify: {
-    customVariables: ["~/assets/common.scss"],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-  /*
-   ** Build configuration
-   */
+
+  // ビルドする前にロードするプラグイン
+  plugins: [
+  ],
+
+  // コンポーネントを自動で読み込むか
+  components: true,
+
+  // dev時、build時に読み込むモジュール
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build', '@nuxtjs/dotenv'
+  ],
+
+  // 一般的なモジュール
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/moment',
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy'
+  ],
+
+  // ビルドの設定
   build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/
-        });
-      }
-    }
   }
-};
+}
