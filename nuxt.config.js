9a3d0a0a1072d332b43d1ac8d6cdc63a30589bc3
Satake <satake00157@gmail.com>
2021-03-09 13:37:39 +0900

    [WIP]動きません

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{ src: undefined }],
+  plugins: [],
   /*
    ** Nuxt.js dev-modules
    */
@@ -64,6 +64,15 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    // extend(config, ctx) {}
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+      }
+    }
   }
 };
