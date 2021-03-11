61d721e9faf777ea2aa85a3ab077530b3d506135
Satake <satake00157@gmail.com>
2021-03-11 23:08:38 +0900

    モックアップをbuefyの設定に近づけるために 3:9の割合で、URLのボタンとデータ本体の部分に分けている

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <section class="main-content">
+      <Header />
+      <div class="container">
+        <div class="columns">
+          <div class="column is-3 section">
+            <navigation />
+          </div>
+          <div class="column is-9 section">
+            <Nuxt />
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import navigation from '~/components/navigation'
+import Header from '~/components/header'
+import Footer from '~/components/footer'
+export default {
+  components: {
+    navigation,
+    Header,
+    Footer
+  }
+}
+</script>
