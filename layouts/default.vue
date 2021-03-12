aa7491b5d4d747d58c40266cc188f49d58766258
Satake <satake00157@gmail.com>
2021-03-12 10:39:44 +0900

    layoutのdefaultが完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,28 +2,17 @@
   <div id="app">
     <section class="main-content">
       <Header />
-      <div class="container">
-        <div class="columns">
-          <div class="column is-3 section">
-            <navigation />
-          </div>
-          <div class="column is-9 section">
-            <Nuxt />
-          </div>
-        </div>
-      </div>
+      <Nuxt />
     </section>
     <Footer />
   </div>
 </template>
 
 <script>
-import navigation from '~/components/navigation'
 import Header from '~/components/header'
 import Footer from '~/components/footer'
 export default {
   components: {
-    navigation,
     Header,
     Footer
   }
