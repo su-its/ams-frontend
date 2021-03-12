0e5f38ffdfce0f6caf79231d50099fd64f5b3184
Satake <satake00157@gmail.com>
2021-03-13 04:14:36 +0900

    コメント化したものを消すのを忘れていた

--- layouts/error.vue
+++ layouts/error.vue
@@ -11,30 +11,11 @@
       </div>
     </div>
   </div>
-  <!--
-    </h1>
-    <h1 v-else-if="error.statusCode === 500">
-      サーバ内でエラーが発生しています<br>
-      システム管理者に報告してください
-    </h1>
-    <h1 v-else-if="error.statusCode === 502">
-      不正なアクセスです
-    </h1>
-    <h1 v-else-if="error.statusCode === 503">
-      サービスが使えません<br>
-      システム管理者に報告してください
-    </h1>
-    <h1 v-else>
-      何かのエラーが発生しています<br>
-      システム管理者に報告してください<br>
-      エラーコード: {{ error.statusCode }}<br>
-      エラーメッセージ: {{ error.message }}
-    </h1> -->
 </template>
 
 <script>
 import navigation from '~/components/navigation'
-import errorCard from '~/components/error_card'
+import errorCard from '~/components/errorCard'
 export default {
   components: {
     navigation,
