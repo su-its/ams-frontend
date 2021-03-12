ce5cfeb454eb9c0314e36483c6d8c3da994bc2b3
Satake <satake00157@gmail.com>
2021-03-13 00:52:21 +0900

    404だけのエラーコンポーネント

---
+++ components/error_card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="section">
+    <div class="card">
+      <div class="card-header">
+        <p class="card-header-title">
+          404 Not Found
+        </p>
+      </div>
+      <div class="card-content">
+        申し訳ございません。<br>
+        指定されたファイルまたはディレクトリは存在いたしません。
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
