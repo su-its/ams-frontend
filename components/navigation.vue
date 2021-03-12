80cd3ca80db7af3179f7235a9201cafac388c7d7
Satake <satake00157@gmail.com>
2021-03-12 10:39:24 +0900

    一応navigationバー完成

---
+++ components/navigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-menu>
+    <b-menu-list label="Navigation">
+      <b-menu-item
+        icon="home-circle"
+        label="現在の在室状況"
+        :active="isActiveRoutePage"
+        tag="NuxtLink"
+        to="/"
+      />
+      <b-menu-item
+        icon="table-large"
+        label="入退室ログの表示"
+        :active="isActiveLogPage"
+        tag="NuxtLink"
+        to="/log"
+      />
+      <b-menu-item
+        icon="information"
+        label="このシステムについて"
+        :active="isActiveInfoPage"
+        tag="NuxtLink"
+        to="/about"
+      />
+    </b-menu-list>
+  </b-menu>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isActiveRoutePage: this.$route.name === 'index',
+      isActiveLogPage: this.$route.name === 'log',
+      isActiveInfoPage: this.$route.name === 'about'
+    }
+  }
+}
+</script>
