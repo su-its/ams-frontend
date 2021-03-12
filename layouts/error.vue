eb15ab627d97a1dee7195a16aaf23f120483c60a
Satake <satake00157@gmail.com>
2021-03-13 01:15:18 +0900

    エラーに関する記載が長すぎるのでコンポーネント化した

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,22 +1,17 @@
 <template>
-  <div id="app">
-    <h1 v-if="error.statusCode === 400">
-      認証されていません
-    </h1>
-    <h1 v-else-if="error.statusCode === 401">
-      認証してください
-    </h1>
-    <h1 v-else-if="error.statusCode === 403">
-      アクセスすることは出来ません
-    </h1>
-    <h1 v-else-if="error.statusCode === 404">
-      存在しないページです
-    </h1>
-    <h1 v-else-if="error.statusCode === 407">
-      プロキシ認証してください
-    </h1>
-    <h1 v-else-if="error.statusCode === 408">
-      タイムアウトです
+  <div class="container">
+    <div class="columns">
+      <div class="column is-3 section">
+        <navigation />
+      </div>
+      <div class="column is-9 section">
+        <div>
+          <errorCard :error="error" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
     </h1>
     <h1 v-else-if="error.statusCode === 500">
       サーバ内でエラーが発生しています<br>
@@ -34,15 +29,17 @@
       システム管理者に報告してください<br>
       エラーコード: {{ error.statusCode }}<br>
       エラーメッセージ: {{ error.message }}
-    </h1>
-    <div class="buttons">
-      <b-button size="is-medium" icon-left="home" label="初期ページ" tag="NuxtLink" to="/" />
-    </div>
-  </div>
+    </h1> -->
 </template>
 
 <script>
+import navigation from '~/components/navigation'
+import errorCard from '~/components/error_card'
 export default {
+  components: {
+    navigation,
+    errorCard
+  },
   props: {
     error: {
       type: Object,
