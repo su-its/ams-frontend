60ed59dadf33d82ad7bc05bda11b12da3d66cab0
Satake <satake00157@gmail.com>
2021-03-15 15:40:08 +0900

    CSVダウンロード部が将来複雑化(パラメータが) する可能性を考慮し、コンポーネント化 ついでに、log.vueが簡素化

---
+++ components/CSVDownload.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <p class="card-header-title">
+        CSV形式でダウンロード
+      </p>
+    </div>
+    <div class="card-content">
+      <p>直近2ヶ月分の入退室ログをCSV形式でダウンロードできます。</p><br>
+      <p>
+        <b-button
+          size="is-medium"
+          icon-left="file-download"
+          label="ダウンロード"
+          type="is-success"
+        />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- pages/log.vue
+++ pages/log.vue
@@ -6,6 +6,8 @@
       </div>
       <div class="column is-9 section">
         <div>
+          <CSVDownload />
+          <br>
           <div class="card">
             <AccessLogCard :log-data="log_data" />
             <b-pagination
@@ -17,21 +19,6 @@
               :range-after="1"
               @change="pagination"
             />
-          </div>
-          <br>
-          <!-- CSVでダウンロードする部分 -->
-          <div class="card">
-            <div class="card-header">
-              <p class="card-header-title">
-                CSV形式でダウンロード
-              </p>
-            </div>
-            <div class="card-content">
-              <p>直近2ヶ月分の入退室ログをCSV形式でダウンロードできます。</p><br>
-              <p>
-                <b-button size="is-medium" icon-left="file-download" label="ダウンロード" type="is-success" />
-              </p>
-            </div>
           </div>
         </div>
       </div>
@@ -45,10 +32,12 @@ import nuxtend from 'nuxtend'
 import CommonMixin from '~/plugins/common'
 import Navigation from '~/components/Common/Navigation'
 import AccessLogCard from '~/components/AccessLogCard'
+import CSVDownload from '~/components/CSVDownload'
 export default nuxtend({
   components: {
     Navigation,
-    AccessLogCard
+    AccessLogCard,
+    CSVDownload
   },
   mixins: [CommonMixin],
   data () {
