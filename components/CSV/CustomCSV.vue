83745efde098b185ddd2cdb329ced0c6a506a98f
Satake <satake00157@gmail.com>
2021-03-15 23:08:49 +0900

    時間指定の部分がめっちゃ長かったのでコンポーネント化

---
+++ components/CSV/CustomCSV.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <b-field grouped>
+      <b-field
+        label="集計開始日"
+        expanded
+      >
+        <b-datepicker
+          v-model="start_date"
+          locale="ja-JP"
+          placeholder="集計開始日を指定してください"
+          icon="calendar-today"
+          :append-to-body="true"
+        >
+
+          <b-button
+            label="クリア"
+            type="is-danger"
+            icon-left="close"
+            outlined
+            @click="start_date = null"
+          />
+
+        </b-datepicker>
+      </b-field>
+      <b-field
+        label="集計終了日"
+        expanded
+      >
+        <b-datepicker
+          v-model="end_date"
+          locale="ja-JP"
+          placeholder="集計終了日を指定してください"
+          icon="calendar-today"
+          :append-to-body="true"
+          aria-expanded
+        >
+          <b-button
+            label="本日"
+            type="is-primary"
+            icon-left="calendar-today"
+            @click="end_date=new Date()"
+          />
+
+          <b-button
+            label="クリア"
+            type="is-danger"
+            icon-left="close"
+            outlined
+            @click="end_date = null"
+          />
+        </b-datepicker>
+      </b-field>
+    </b-field>
+    <b-button
+      size="is-medium"
+      icon-left="file-download"
+      label="ダウンロード"
+      type="is-info"
+      @click="getCSV()"
+    />
+  </div>
+</template>
+
+<script>
+import common from '~/plugins/common'
+import nuxtend from 'nuxtend'
+export default nuxtend({
+  mixins:[common],
+  methods: {
+    getCSV () {
+      this.download()
+    }
+  },
+})
+</script>
--- components/CSVDownload.vue
+++ components/CSVDownload.vue
@@ -15,80 +15,13 @@
           label="2か月分"
           icon="file-download"
         >
-          <p>直近2ヶ月分の入退室ログをCSV形式でダウンロードできます。</p><br>
-          <p>
-            <b-button
-              size="is-medium"
-              icon-left="file-download"
-              label="ダウンロード"
-              type="is-success"
-              @click="download()"
-            />
-          </p>
+        <DefaultCSV />
         </b-tab-item>
         <b-tab-item
           label="自分で設定する"
           icon="download-multiple"
         >
-          <b-field grouped>
-            <b-field
-              label="集計開始日"
-              expanded
-            >
-              <b-datepicker
-                v-model="start_date"
-                locale="ja-JP"
-                placeholder="集計開始日を指定してください"
-                icon="calendar-today"
-                :append-to-body="true"
-              >
-
-                <b-button
-                  label="クリア"
-                  type="is-danger"
-                  icon-left="close"
-                  outlined
-                  @click="start_date = null"
-                />
-
-              </b-datepicker>
-            </b-field>
-            <b-field
-              label="集計終了日"
-              expanded
-            >
-              <b-datepicker
-                v-model="end_date"
-                locale="ja-JP"
-                placeholder="集計終了日を指定してください"
-                icon="calendar-today"
-                :append-to-body="true"
-                aria-expanded
-              >
-                <b-button
-                  label="本日"
-                  type="is-primary"
-                  icon-left="calendar-today"
-                  @click="end_date=new Date()"
-                />
-
-                <b-button
-                  label="クリア"
-                  type="is-danger"
-                  icon-left="close"
-                  outlined
-                  @click="end_date = null"
-                />
-              </b-datepicker>
-            </b-field>
-          </b-field>
-          <b-button
-            size="is-medium"
-            icon-left="file-download"
-            label="ダウンロード"
-            type="is-info"
-            @click="download()"
-          />
+        <CustomCSV />
         </b-tab-item>
       </b-tabs>
     </div>
@@ -96,7 +29,13 @@
 </template>
 
 <script>
+import DefaultCSV from '~/components/CSV/DefaultCSV'
+import CustomCSV from '~/components/CSV/CustomCSV'
 export default {
+  components: {
+    DefaultCSV,
+    CustomCSV
+  },
   data() {
     return {
       start_date: null,
