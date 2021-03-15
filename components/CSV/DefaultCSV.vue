cd602af4b81055b9ef5635b48e23ea70b8535fb6
Satake <satake00157@gmail.com>
2021-03-15 22:56:44 +0900

    二か月分のCSVのvueをコンポーネント化

---
+++ components/CSV/DefaultCSV.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>直近2ヶ月分の入退室ログをCSV形式でダウンロードできます。</p><br>
+    <p>
+      <b-button
+        size="is-medium"
+        icon-left="file-download"
+        label="ダウンロード"
+        type="is-success"
+        @click="localDownload()"
+      />
+    </p>
+  </div>
+</template>
+
+<script>
+import common from '~/plugins/common'
+import nuxtend from 'nuxtend'
+export default nuxtend({
+  mixins:[common],
+  methods: {
+    localDownload () {
+      this.download()
+    }
+  },
+})
+</script>
+
