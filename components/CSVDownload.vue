220f7deeedf9116485b99c18265b19b54dff32e8
Satake <satake00157@gmail.com>
2021-03-15 17:36:29 +0900

    CSVダウンロードを自分で期間が選べるようにした 少しUIにずれが生じているので直したい
    構造が複雑になってるのでコンポーネント化しても良いかもしれない

--- components/CSVDownload.vue
+++ components/CSVDownload.vue
@@ -6,19 +6,101 @@
       </p>
     </div>
     <div class="card-content">
-      <p>直近2ヶ月分の入退室ログをCSV形式でダウンロードできます。</p><br>
-      <p>
-        <b-button
-          size="is-medium"
-          icon-left="file-download"
-          label="ダウンロード"
-          type="is-success"
-        />
-      </p>
+      <b-tabs
+        v-model="activeTab"
+        type="is-toggle"
+        expanded
+      >
+        <b-tab-item
+          label="2か月分"
+          icon="file-download"
+        >
+          <p>直近2ヶ月分の入退室ログをCSV形式でダウンロードできます。</p><br>
+          <p>
+            <b-button
+              size="is-medium"
+              icon-left="file-download"
+              label="ダウンロード"
+              type="is-success"
+            />
+          </p>
+        </b-tab-item>
+        <b-tab-item
+          label="自分で設定する"
+          icon="download-multiple"
+        >
+          <b-field grouped>
+            <b-field
+              label="集計開始日"
+              expanded
+            >
+              <b-datepicker
+                v-model="start_date"
+                locale="ja-JP"
+                placeholder="集計開始日を指定してください"
+                icon="calendar-today"
+                :append-to-body="true"
+              >
+
+                <b-button
+                  label="クリア"
+                  type="is-danger"
+                  icon-left="close"
+                  outlined
+                  @click="start_date = null"
+                />
+
+              </b-datepicker>
+            </b-field>
+            <b-field
+              label="集計終了日"
+              expanded
+            >
+              <b-datepicker
+                v-model="end_date"
+                locale="ja-JP"
+                placeholder="集計終了日を指定してください"
+                icon="calendar-today"
+                :append-to-body="true"
+                aria-expanded
+              >
+                <b-button
+                  label="本日"
+                  type="is-primary"
+                  icon-left="calendar-today"
+                  @click="end_date=new Date()"
+                />
+
+                <b-button
+                  label="クリア"
+                  type="is-danger"
+                  icon-left="close"
+                  outlined
+                  @click="end_date = null"
+                />
+              </b-datepicker>
+            </b-field>
+          </b-field>
+          <b-button
+            size="is-medium"
+            icon-left="file-download"
+            label="ダウンロード"
+            type="is-primary"
+          />
+        </b-tab-item>
+      </b-tabs>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      start_date: null,
+      end_date: null,
+      activeTab: 0
+    }
+  }
+}
 </script>
