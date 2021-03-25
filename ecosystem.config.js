67ef1bb953e5026e07bb73f4b5ed7979fd1f615c
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-26 02:27:47 +0900

    prestartを追加。npm start時に必ずnuxt buildが走るようにした

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'ams-frontend',
     script: 'npm',
-    args: 'run dev',
+    args: 'start',
     watch: false,
     restart_delay: 1000,
     log_date_format: 'YYYY-MM-DD HH:mm Z',
