49ee919dcbe1e470c38dd617dffc44361efe1b01
equal-l2 <eng.equall2@gmail.com>
2021-03-26 19:02:13 +0900

    ecosystem.config.js: HOST を 0.0.0.0 に指定

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
     watch: false,
     restart_delay: 1000,
     log_date_format: 'YYYY-MM-DD HH:mm Z',
-    merge_logs: true
+    merge_logs: true,
+    env: {
+      HOST: '0.0.0.0'
+    }
   }]
 }
