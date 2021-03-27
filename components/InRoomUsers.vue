23e62f29aa67cfcf6cf2eb67d0be4a79a456c818
equal-l2 <eng.equall2@gmail.com>
2021-03-27 18:49:49 +0900

    在室状況画面を調整
    - 「静岡大学 ITソルーション室」の表記を「在室状況」に置換
    - 他画面に比べて表示領域が小さかったのを修正(モックアップと構造を合わせた)

--- components/InRoomUsers.vue
+++ components/InRoomUsers.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="section">
-    <div class="card">
-      <div class="card-header">
-        <p class="card-header-title">
-          静岡大学 ITソルーション室
-        </p>
-      </div>
-      <div class="card-content">
-        <div v-if="roomUser.length != 0" id="user-count" class="content">
-          現在、 {{ roomUser.length }} 人 が在室中です。
-          <div v-for="user in roomUser" :key="user.id" class="has-shadow">
-            <User :user="user" />
-          </div>
-        </div>
-        <div v-else class="has-shadow">
-          現在、不在です。
+  <div class="card">
+    <div class="card-header">
+      <p class="card-header-title">
+        在室状況
+      </p>
+    </div>
+    <div class="card-content">
+      <p v-if="roomUser.length == 0" class="content">
+        現在、誰も在室していません。
+      </p>
+      <p v-else class="content">
+        現在、 {{ roomUser.length }} 人 が在室中です。
+      </p>
+      <div class="content">
+        <div v-for="user in roomUser" :key="user.id">
+          <User :user="user" />
         </div>
       </div>
     </div>
