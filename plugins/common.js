183fdc9a7ccb30803fd60276a4a93993ac585400
equal-l2 <eng.equall2@gmail.com>
2021-04-08 01:34:42 +0900

    nuxtendを削除
    asyncData内でthisが使えないことは公式Docにも記載があるので逆らうのはよくない
    またnuxtendは1年以上更新がなくスターも少ない

--- plugins/common.js
+++ plugins/common.js
@@ -1,54 +1,6 @@
-import _ from 'lodash'
 import { Parser } from 'json2csv'
-
 export default {
   methods: {
-    /**
-     * dispatchされたデータをqueryにする関数
-     * @param {*} data formであることが多い。
-     * @returns {string} query化されたデータが帰ってきます ex: hoge=111&tmp=333 一番最初の?がつかないことに注意してください
-     */
-    convertParamsToQuery (data) {
-      let query = ''
-      _.each(data, (value, key) => {
-        if (key !== 'page' && value !== '' && value !== undefined) {
-          query += `&${key}=${value}`
-        }
-      })
-      return query
-    },
-
-    /**
-     * dispatchする前にデータ(form)を一つの変数にする関数
-     * v-model等を使う場合はこれを使うことで、関数を省略できる
-     * @param {Object} form formデータ
-     * @returns {*} 変数化されて帰ってきます。主にstoreのactions向けに使います。
-     */
-    convertDaraToParams (form) {
-      const data = _.cloneDeep(form)
-      return _.pickBy(
-        _.transform(data, function (result, value, key) {
-          result[key] = _.trim(value)
-        }),
-        v => !_.isEmpty(v)
-      )
-    },
-
-    /**
-     * pageの情報を追加、あるいはチェックするメソッド
-     * @param {*} data 数字のデータ(違うデータを入れた場合1扱いになります)
-     * @returns {Number} page 何もなければ1、そうでなければ該当するページが帰ってきます
-    */
-    checkNullPageData (data) {
-      let page = 1
-      if (_.isNumber(data)) {
-        page = data
-      } else {
-        page = 1
-      }
-      return page
-    },
-
     /**
      * バックエンドから指定期間の入退室ログを取得し
      * CSVファイルに変換してダウンロードする
