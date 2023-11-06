f05d1faf9795d5f78589b10b381fe8a3eabdf0d4
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-11-06 11:03:10 +0900

    設定の修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,33 @@
-import * as packageJson from './package.json'
+import * as packageJson from "./package.json";
+import { defineNuxtConfig } from "nuxt/config";
 
-export default {
+export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+  experimental: {
+    payloadExtraction: true,
+  },
   server: {
     // ポート番号を指定
-    port: process.env.APP_PORT
+    port: process.env.APP_PORT,
   },
-
-  axios: {
-    proxy: true,
-    prefix: '/api'
-  },
-
   env: {
     // このソフトのバージョンを設定(画面に表示するため)
-    PKG_VERSION: packageJson.version || 'バージョン情報の取得に失敗しました'
+    PKG_VERSION: packageJson.version || "バージョン情報の取得に失敗しました",
   },
 
   proxy: {
     // バックエンドAPIをプロクシする
-    '/api/': {
+    "/api/": {
       target: process.env.API_URL,
       pathRewrite: {
         // 余計なパスを取り除く
-        '^/api/': ''
+        "^/api/": "",
       },
-      xfwd: true // x-forwarded-for を付ける
-    }
+      xfwd: true, // x-forwarded-for を付ける
+    },
   },
 
   // ssr: true ユニバーサルモード(サーバサイドレンダリングあり)
@@ -33,67 +35,66 @@ export default {
   ssr: false,
 
   // ページヘッダ
-  head: {
-    title: 'ITS-AMS コントロールパネル',
-    titleTemplate: '%s - ITS-AMS コントロールパネル',
-    htmlAttrs: {
-      lang: 'ja'
+  app: {
+    head: {
+      title: "ITS-AMS コントロールパネル",
+      titleTemplate: "%s - ITS-AMS コントロールパネル",
+      htmlAttrs: {
+        lang: "ja",
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+        { hid: "description", name: "description", content: "" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
   // データの入出力を全てJSONで行うように設定
   headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 100000000,
+    },
+    define: {
+      "process.env.DEBUG": false,
+    },
+    optimizeDeps: { exclude: ["fsevents"] },
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
   },
 
   // CSS
-  css: ['~/assets/common.scss'],
+  css: ["@/assets/common.scss"],
 
   // ビルドする前にロードするプラグイン
-  plugins: [
-  ],
-
-  // コンポーネントを自動で読み込むか
-  components: true,
-
-  // dev時、build時に読み込むモジュール
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxtjs/dotenv'
-  ],
+  plugins: [],
 
   // 一般的なモジュール
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/moment',
-    // https://go.nuxtjs.dev/buefy
-    'nuxt-buefy'
-  ],
+  modules: [],
 
   moment: {
     // momentで日本である事を書く必要がなくなった
-    locales: ['ja']
+    locales: ["ja"],
   },
 
   // ビルドの設定
   build: {
     cache: true,
-    parallel: true
+    parallel: true,
   },
 
   telemetry: false,
 
   render: {
     // SSEと圧縮の相性が悪いのでオフにしておく
-    compressor: false
-  }
-}
+    compressor: false,
+  },
+});
