d1652c208c8143f20745a0944d1425f9c155cc6d
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-11-06 23:19:14 +0900

    fontawesomeを導入

--- components/Common/Navigation.vue
+++ components/Common/Navigation.vue
@@ -1,36 +1,37 @@
 <template>
-  <b-menu>
-    <b-menu-list label="Navigation">
-      <b-menu-item
-        class="menu-items"
-        icon="home-circle"
-        label="現在の在室状況"
-        :active="isActiveRoutePage"
-        tag="NuxtLink"
-        to="/"
-      />
-      <b-menu-item
-        class="menu-items"
-        icon="table-large"
-        label="入退室ログの表示"
-        :active="isActiveLogPage"
-        tag="NuxtLink"
-        to="/log"
-      />
-      <b-menu-item
-        class="menu-items"
-        icon="information"
-        label="このシステムについて"
-        :active="isActiveInfoPage"
-        tag="NuxtLink"
-        to="/about"
-      />
-    </b-menu-list>
-  </b-menu>
+  <aside class="menu">
+    <ul class="menu-list">
+      <li>
+        <a class="isActiveRoutePage"><font-awesome-icon icon="house" />
+          <NuxtLink to="/">現在の在室状況</NuxtLink>
+        </a>
+      </li>
+      <li>
+        <a class="isActiveLogPage"><font-awesome-icon icon="table-cells-large" />
+          <NuxtLink to="/log">入退室ログの表示</NuxtLink>
+        </a>
+      </li>
+      <li><a class="isActiveInfoPage"><font-awesome-icon icon="info" />
+          <NuxtLink to="/about">このシステムについて</NuxtLink>
+        </a>
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <script setup>
-const isActiveRoutePage = useRoute().name === 'index'
-const isActiveLogPage = useRoute().name === 'log'
-const isActiveInfoPage = useRoute().name === 'about'
+onMounted(() => {
+  if (useRoute().name === "index") {
+    let elem = document.getElementsByClassName("isActiveRoutePage");
+    elem[0].classList.toggle("is-active");
+  }
+  if (useRoute().name === "log") {
+    let elem = document.getElementsByClassName("isActiveLogPage");
+    elem[0].classList.toggle("is-active");
+  }
+  if (useRoute().name === "about") {
+    let elem = document.getElementsByClassName("isActiveInfoPage");
+    elem[0].classList.toggle("is-active");
+  }
+});
 </script>
