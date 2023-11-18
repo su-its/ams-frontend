eba7434ab659473edddc5c256d4e64a6cf41f744
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-11-18 12:44:09 +0900

    css切り替え以外完了

--- components/CSV/CustomCSV.vue
+++ components/CSV/CustomCSV.vue
@@ -8,57 +8,20 @@
         class="label"
         expanded
       >集計開始日
-        <button
-          class="is-danger"
-          icon-left="close"
-          outlined
-          @click="startDate = null"
-        >クリア</button>
-        <button
-          ref="startDateTrigger"
-          type="button"
-        >集計開始日を指定してください</button>
+        <a ref="startDateTrigger" />
       </label>
-      <!--<b-field
-        label="集計終了日"
+      <label
+        class="label"
         expanded
-      >
-        <b-datepicker
-          v-model="endDate"
-          locale="ja-JP"
-          placeholder="集計終了日を指定してください"
-          icon="calendar-today"
-          :append-to-body="true"
-          :min-date="minDate"
-          :max-date="maxDate"
-          aria-expanded
-        >
-          <b-button
-            label="本日"
-            type="is-primary"
-            icon-left="calendar-today"
-            @click="endDate=new Date()"
-          />
-
-          <b-button
-            label="クリア"
-            type="is-danger"
-            icon-left="close"
-            outlined
-            @click="endDate = null"
-          />
-        </b-datepicker>
-      </b-field>
+      >集計終了日
+        <a ref="endDateTrigger" />
+      </label>
     </div>
     <br>
-    <b-button
-      size="is-medium"
-      icon-left="file-download"
-      label="ダウンロード"
-      type="is-info"
+    <button
+      class="button is-medium is-info"
       @click="getCSV()"
-    />-->
-    </div>
+    ><font-awesome-icon icon="file-arrow-down" />ダウンロード</button>
   </div>
 </template>
 
@@ -69,42 +32,49 @@ const startDate = ref(new Date());
 const endDate = ref(new Date());
 const snackbar = ref();
 const startDateTrigger = ref(false);
+const endDateTrigger = ref(false);
 // CSVのファイル名が年2桁なのでambiguousにならないようにしておく
 const minDate = new Date("2000-01-01");
 const maxDate = new Date("2099-12-31");
 
-// const calender = bulmaCalendar.attach(startDateTrigger.value, {
-//   startDate: new Date(),
-// });
-// calender.on("select", (e) => (startDate.value = e.start || null));
+onMounted(() => {
+  const startCalender = bulmaCalendar.attach(startDateTrigger.value, {
+    startDate: new Date(),
+    lang: "ja",
+    type: "date",
+  })[0];
+  startCalender.on("select", (e) => (startDate.value = e.start || null));
+  const endCalender = bulmaCalendar.attach(endDateTrigger.value, {
+    startDate: new Date(),
+    lang: "ja",
+    type: "date",
+  })[0];
+  endCalender.on("select", (e) => (endDate.value = e.start || null));
+});
 
-// const startDate = computed(() => {
-//   return startDate.value.toLocaleDateString()
-// })
-
-// function getCSV() {
-//   const startDate_ = moment(startDate.value);
-//   const endDate_ = moment(endDate.value);
-//   if (!startDate_.isValid()) {
-//     snackbar.value.open({
-//       message: "開始日を設定してください",
-//       type: "is-warning",
-//       position: "is-top",
-//     });
-//   } else if (!endDate_.isValid()) {
-//     snackbar.value.open({
-//       message: "終了日を設定してください",
-//       type: "is-warning",
-//       position: "is-top",
-//     });
-//   } else if (startDate_ > endDate_) {
-//     snackbar.value.open({
-//       message: "開始日は必ず終了日より前に設定してください",
-//       type: "is-warning",
-//       position: "is-top",
-//     });
-//   } else {
-//     UseUtils().download(startDate_, endDate_);
-//   }
-// }
+function getCSV() {
+  const startDate_ = moment(startDate.value);
+  const endDate_ = moment(endDate.value);
+  if (!startDate_.isValid()) {
+    snackbar.value.open({
+      message: "開始日を設定してください",
+      type: "is-warning",
+      position: "is-top",
+    });
+  } else if (!endDate_.isValid()) {
+    snackbar.value.open({
+      message: "終了日を設定してください",
+      type: "is-warning",
+      position: "is-top",
+    });
+  } else if (startDate_ > endDate_) {
+    snackbar.value.open({
+      message: "開始日は必ず終了日より前に設定してください",
+      type: "is-warning",
+      position: "is-top",
+    });
+  } else {
+    // UseUtils().download(startDate_, endDate_);
+  }
+}
 </script>
