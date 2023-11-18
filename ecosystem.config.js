690d7b61aeed6089815fd8f5fc10c316a2694c8b
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-11-18 20:18:41 +0900

    ecosystemのパラメータを変更

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'ams-frontend',
     script: 'npm',
-    args: 'start',
+    args: 'preview',
     watch: false,
     restart_delay: 1000,
     log_date_format: 'YYYY-MM-DD HH:mm Z',
