082ee83c96b916d52af9615add63076a8e7591c2
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2023-11-18 19:06:18 +0900

    タブの切り替えを出来るようにした

--- components/CSV/CSVDownload.vue
+++ components/CSV/CSVDownload.vue
@@ -8,14 +8,39 @@
     <div class="card-content">
       <div class="tabs is-boxed is-toggle">
         <ul>
-          <li class="is-active"><a><font-awesome-icon icon="file-arrow-down" />3か月分</a>
-            <CSVDefaultCSV />
+          <li v-bind:class="{ 'is-active': isActive == 'default' }"><a v-on:click="isActive = 'default'"><font-awesome-icon icon="file-arrow-down" />3か月分</a>
           </li>
-          <li><a><font-awesome-icon icon="download" />自分で設定する</a>
-            <CSVCustomCSV />
+          <li v-bind:class="{ 'is-active': isActive == 'custom' }"><a v-on:click="isActive = 'custom'"><font-awesome-icon icon="download" />自分で設定する</a>
           </li>
         </ul>
       </div>
     </div>
+    <div class="tab-contents">
+      <div
+        class="content"
+        v-bind:class="{ 'is-active': isActive == 'default'}"
+      >
+        <CSVDefaultCSV />
+      </div>
+      <div
+        class="content"
+        v-bind:class="{ 'is-active': isActive == 'custom'}"
+      >
+        <CSVCustomCSV />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const isActive = ref("default");
+</script>
+
+<style scoped>
+.tab-contents .content {
+  display: none;
+}
+.tab-contents .content.is-active {
+  display: block;
+}
+</style>
