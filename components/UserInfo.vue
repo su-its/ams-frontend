063426cbb5f3ac7cf12378aa5a045dd8bb9a05e0
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2024-01-06 11:34:40 +0900

    画像の非表示の問題を解決

--- components/UserInfo.vue
+++ components/UserInfo.vue
@@ -4,7 +4,7 @@
       <div class="media">
         <div class="media-left">
           <figure class="image is-48x48">
-            <img src="noimage.png">
+            <img src="/noimage.png">
           </figure>
         </div>
         <div class="media-content">
